32bd53c60a12e32aaaa3db2e85108eb5457cec61
Dobromir Hristov <djdobro@abv.bg>
2017-10-09 22:20:06 +0300

    chore: eslint styling

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import vueVimeoPlayer from './main.js'
 
 function plugin (Vue, options) {
-	Vue.component('vimeo-player', vueVimeoPlayer)
+  Vue.component('vimeo-player', vueVimeoPlayer)
 }
 
 plugin.version = '__VERSION__'
 
 export default plugin
 
-
 export {
-	vueVimeoPlayer
+  vueVimeoPlayer
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
